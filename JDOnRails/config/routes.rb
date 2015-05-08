031086eef2a0f3f92871133c96f81246bc201e80
whiteisnick <whiteisnick@gmail.com>
2015-05-08 13:07:05 -0400

    changed routes back from crazy

--- JDOnRails/config/routes.rb
+++ JDOnRails/config/routes.rb
@@ -20,7 +20,10 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :abouts
+  resources :gigs
+  resources :stories
+  resources :bands
   # Example resource route with options:
   #   resources :products do
   #     member do
