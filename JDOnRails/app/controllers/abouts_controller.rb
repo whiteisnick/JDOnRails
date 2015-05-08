815bbbe453ce4f51d21df9cd9e29e85a67f5fd9d
whiteisnick <whiteisnick@gmail.com>
2015-05-08 13:53:29 -0400

    added CRUD actions

--- JDOnRails/app/controllers/abouts_controller.rb
+++ JDOnRails/app/controllers/abouts_controller.rb
@@ -1,2 +1,51 @@
 class AboutsController < ApplicationController
+
+  def index
+    @abouts = About.all
+  end
+ 
+  def show
+    @about = About.find(params[:id])
+  end
+ 
+  def new
+    @about = About.new
+  end
+ 
+  def edit
+    @about = About.find(params[:id])
+  end
+ 
+  def create
+    @about = About.new(about_params)
+ 
+    if @about.save
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
+ 
+  def update
+    @about = About.find(params[:id])
+ 
+    if @about.update(about_params)
+      redirect_to '/'
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    @about = About.find(params[:id])
+    @about.destroy
+ 
+    redirect_to '/'
+  end
+ 
+  private
+    def about_params
+      params.require(:about).permit(:description, :band_id)
+    end
+
 end
--- JDOnRails/app/controllers/stories_controller.rb
+++ JDOnRails/app/controllers/stories_controller.rb
@@ -1,2 +1,51 @@
 class StoriesController < ApplicationController
+
+  def index
+    @stories = Story.all
+  end
+ 
+  def show
+    @story = Story.find(params[:id])
+  end
+ 
+  def new
+    @story = Story.new
+  end
+ 
+  def edit
+    @story = Story.find(params[:id])
+  end
+ 
+  def create
+    @story = Story.new(story_params)
+ 
+    if @story.save
+      redirect_to "/"
+    else
+      render 'new'
+    end
+  end
+ 
+  def update
+    @story = Story.find(params[:id])
+ 
+    if @story.update(story_params)
+      redirect_to '/'
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+ 
+    redirect_to "/"
+  end
+ 
+  private
+    def story_params
+      params.require(:story).permit(:news, :band_id)
+    end
+
 end
